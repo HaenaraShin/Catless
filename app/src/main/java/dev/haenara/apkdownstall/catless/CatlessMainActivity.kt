3d79c28cb6d8a775696dabc5066fec0166e9d72b
Haenala Shin <hamster12345@gmail.com>
2019-11-12 20:14:20 +0900

    Add basic android project

---
+++ app/src/main/java/dev/haenara/apkdownstall/catless/CatlessMainActivity.kt
@@ -0,0 +1,18 @@
+package dev.haenara.apkdownstall.catless
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatlessMainActivity : AppCompatActivity() {
+    // TODO define super class 'Basic Activity'
+    // TODO add DataBinding logic
+    // TODO add ViewModel
+    // TODO create repository to get cats list
+    // TODO create ApiService to get cats via TheCatAPI
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
