24f29263c6a8563629656c7272864af9999c92fb
haenaraShin <hamster12345@gmail.com>
2019-11-17 22:53:24 +0900

    Add ViewModel

--- app/src/main/java/dev/haenara/catless/view/CatlessMainActivity.kt
+++ app/src/main/java/dev/haenara/catless/view/CatlessMainActivity.kt
@@ -2,19 +2,25 @@ package dev.haenara.catless.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import dev.haenara.catless.R
+import dev.haenara.catless.databinding.ActivityMainBinding
+import dev.haenara.catless.viewmodel.CatViewModel
 
 class CatlessMainActivity : AppCompatActivity() {
-    // TODO define super class 'Basic Activity'
-    // TODO add DataBinding logic
-    // TODO add ViewModel
     // TODO create repository to get cats list
     // TODO create ApiService to get cats via TheCatAPI
 
+    lateinit var mCatViewModel: CatViewModel
+    lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mBinding.lifecycleOwner = this
 
-
+        mCatViewModel = ViewModelProviders.of(this).get(CatViewModel::class.java)
     }
 }
