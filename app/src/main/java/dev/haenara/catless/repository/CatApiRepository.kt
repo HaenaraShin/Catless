b0e0b6d107c1dfc57e30060f9375c32848b25cc0
Haenala Shin <hamster12345@gmail.com>
2019-11-18 20:23:30 +0900

    Add repository to get cat image from api

---
+++ app/src/main/java/dev/haenara/catless/repository/CatApiRepository.kt
@@ -0,0 +1,62 @@
+package dev.haenara.catless.repository
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import dev.haenara.catless.api.CatRetrofit
+import dev.haenara.catless.model.Cat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+object CatRepository {
+    private val TAG = "Catless::CatRepository"
+    private val dataSet = ArrayList<Cat>()
+
+    fun getDataSet(): MutableLiveData<List<Cat>> {
+        setCats()
+        val data = MutableLiveData<List<Cat>>()
+        data.value = dataSet
+        return data
+    }
+
+    fun getDataSet(callback: ()-> Unit) {
+        getCat(object: CatReceivedCallback{
+            override fun onCatReceived(cat: Cat?) {
+                cat?.let {
+                    dataSet.add(it)
+                }
+                callback()
+            }
+        })
+    }
+
+    private fun setCats(){
+        getCat(object: CatReceivedCallback{
+            override fun onCatReceived(cat: Cat?) {
+                cat?.let {
+                    dataSet.add(it)
+                }
+            }
+        })
+    }
+
+    private fun getCat(callback: CatReceivedCallback){
+        CatRetrofit.getService().requestCatImage().enqueue(object:
+            Callback<List<Cat>> {
+            override fun onFailure(call: Call<List<Cat>>, t: Throwable) {
+                Log.e(TAG, t.message ?: "error occured.")
+            }
+
+            override fun onResponse(call: Call<List<Cat>>, response: Response<List<Cat>>) {
+                response.body()?.get(0)?.let {
+                    callback.onCatReceived(it)
+                }
+            }
+        })
+    }
+}
+
+interface CatReceivedCallback {
+    fun onCatReceived(cat: Cat?)
+}
--- app/src/main/java/dev/haenara/catless/viewmodel/CatViewModel.kt
+++ app/src/main/java/dev/haenara/catless/viewmodel/CatViewModel.kt
@@ -1,6 +1,17 @@
 package dev.haenara.catless.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.haenara.catless.model.Cat
+import dev.haenara.catless.repository.CatRepository
 
 class CatViewModel :ViewModel(){
+    private val mCats = CatRepository.getDataSet()
+    public fun getCatLivedata(): MutableLiveData<List<Cat>> = mCats
+
+    fun addCat(callback: ()-> Unit) {
+        CatRepository.getDataSet(callback)
+    }
+
+
 }
